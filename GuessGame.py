4f272af4748052d0ffadddb25b7e2bffb47afd0e
Buffboi2022 <103544734+Buffboi2022@users.noreply.github.com>
2023-02-23 19:08:22 -0600

    Add files via upload

---
+++ GuessGame.py
@@ -0,0 +1,20 @@
+Number= int(input("Hello, I am the Phionex! Choose a number 1-10!"))
+
+if (Number==1):
+    print("You sart to feel wrinkles. You have aged, 60 years! Game over.")
+if (Number==2):
+    print("A Rock falls on you. Game Over!")
+if (Number==3):
+    print("A Dragon Eats you! Game over!")
+if (Number==4):
+    print("A pit of lava appears bellow you feet! Game over!")
+if (Number==5):
+    print("A treasure chest Appears! But it's alive. Game over!")
+if (Number==6):
+    print("A trap door aove you opens, dropping ton of gold! You Win!")
+if (Number==7):
+    print("A load of poisonus arrows are fired at you! Game over!")   
+if (Number==9):
+    print("You cant breathe any more! Game over!")
+if (Number==10):
+    print("A pot of gold appears, but you Trip and Die... Game over!")
